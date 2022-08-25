b1d52f3f28d2bf4021128663d5807d7699881644
AnilKumarN63 <112076665+AnilKumarN63@users.noreply.github.com>
2022-08-25 21:00:07 +0530

    Add files via upload

--- anil.js
+++ anil.js
@@ -2,6 +2,9 @@ function login(){
   var uname=document.getElementById("UserName").value;
   var pass=document.getElementById("Password").value;
   if(uname=="Sweetie" && pass=="Mouni@123"){
-    window.alert("Hello")
+    location.assign("html1.html")
+  }
+  else{
+    window.alert("Hello Type Correct Password otherwise i will kill u")
   }
 }
