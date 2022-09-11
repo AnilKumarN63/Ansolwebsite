744c7d43e4f0913a38fa1c84b4cf0ec729108599
AnilKumarN63 <112076665+AnilKumarN63@users.noreply.github.com>
2022-09-11 16:48:04 +0530

    Add files via upload

--- mouni.js
+++ mouni.js
@@ -2,7 +2,7 @@ function login(){
     var uname=document.getElementById("username").value;
     var pass=document.getElementById("password").value;
     if(uname=="Mounika" && pass=="Mouni@6363"){
-        location.assign("index.html")
+        location.assign("mouni1.html")
     }
     else{
         window.alert("WHO ARE YOU,WITHOUT KNOWING USERNAME AND PASSWORD HOW CAN U LOGIN")
